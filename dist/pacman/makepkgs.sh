f862990552f2f00b1ac26814e710f8e52dade2e5
Darren Edale <darren@equituk.net>
2022-01-31 17:19:33 +0000

    Updated packaging scripts
    - bumped package release for rebuild with fixed missing libqonvice symlink

--- dist/pacman/makepkgs.sh
+++ dist/pacman/makepkgs.sh
@@ -3,7 +3,7 @@ BASEDIR=$(dirname "$(readlink -f makepkgs.sh)")
 cd "${BASEDIR}"
 
 PKG_VERSION=1.9.0
-PKG_RELEASE=1
+PKG_RELEASE=2
 
 echo Building package libqonvince ...
 cd "${BASEDIR}"/libqonvince
